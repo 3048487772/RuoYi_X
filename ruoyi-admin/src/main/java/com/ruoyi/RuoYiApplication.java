1e0e26b7ad6894444c5b03b56bc99ee0ebd50720
MYCOMPUTER\30484 <3048487772@qq.com>
2023-05-15 10:55:59 +0800

    update

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -1,16 +1,17 @@
 package com.ruoyi;
 
 import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.core.util.URLUtil;
 import cn.hutool.extra.spring.SpringUtil;
-import cn.hutool.http.HttpUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.buf.UriUtil;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import java.io.File;
+import java.net.URL;
 
 /**
  * 启动程序
@@ -30,19 +31,38 @@ public class RuoYiApplication {
         String line = "=";
         String success = "|项目启动成功";
         String port = StrUtil.format("|端口:[{}]", SpringUtil.getProperty("server.port"));
+        
         String docUrl = StrUtil.format("http://localhost:{}/{}/doc.html", SpringUtil.getProperty("server.port"), SpringUtil.getProperty("server.servlet.context-path"));
-        docUrl = URLUtil.normalize(docUrl,false,true);
+        docUrl = URLUtil.normalize(docUrl, false, true);
+        
         String profile = StrUtil.format("|配置文件：{}", SpringUtil.getProperty("spring.profiles.active"));
+        
         String doc = StrUtil.format("|文档地址:[{}]", docUrl);
-        int max = NumberUtil.max(line.length(), success.length(), port.length(), doc.length());
-        line = StrUtil.fillAfter(line, '=', max+8);
-       
+        
+        ClassLoader classLoader = RuoYiApplication.class.getClassLoader();
+        
 
+        URL resource = classLoader.getResource("static/index.html");
+        String htmlUrl = null;
+        if (ObjectUtil.isNotNull(resource)) {
+            File file = new File(resource.getFile());
+            if (file.exists()) {
+                htmlUrl = StrUtil.format("|网页地址:[http://localhost:{}/{}/index.html]", SpringUtil.getProperty("server.port"), SpringUtil.getProperty("server.servlet.context-path"));
+                htmlUrl = URLUtil.normalize(htmlUrl, false, true);
+            }
+        }
+
+        int max = NumberUtil.max(line.length(), success.length(), port.length(), doc.length(),StrUtil.length(htmlUrl));
+        line = StrUtil.fillAfter(line, '=', max + 8);
+        
         log.info(line);
         log.info(success);
         log.info(port);
-        log.info(doc);
         log.info(profile);
+        log.info(doc);
+        if (ObjectUtil.isNotEmpty(htmlUrl)) {
+            log.info(htmlUrl);
+        }
         log.info(line);
     }
 
